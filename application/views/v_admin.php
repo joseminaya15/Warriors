f55296d554625e839f0ae96686e6e38534958ed8
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-06-27 18:07:28 -0500

    cambios

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -4,13 +4,13 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible"  content="IE=edge">
     <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <meta name="description"            content=">HPE LAC Value & Growth Channels Warriors">
-    <meta name="keywords"               content=">HPE LAC Value & Growth Channels Warriors">
+    <meta name="description"            content="HPE LAC Value & Growth Channels Warriors">
+    <meta name="keywords"               content="HPE LAC Value & Growth Channels Warriors">
     <meta name="robots"                 content="Index,Follow">
     <meta name="date"                   content="January 25, 2018"/>
     <meta name="language"               content="es">
     <meta name="theme-color"            content="#000000">
-	  <title>>HPE LAC Value & Growth Channels Warriors</title>
+	  <title>HPE LAC Value & Growth Channels Warriors</title>
     <link rel="shortcut icon" href="<?php echo RUTA_IMG?>favicon.png">
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.min.css?v=<?php echo time();?>">
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
@@ -33,7 +33,7 @@
                     <img src="<?php echo RUTA_IMG?>logo/hpe-intel.png">
                 </div>
                 <div class="js-flex--right">
-                    <p>>HPE LAC Value & Growth Channels Warriors</p>
+                    <p>HPE LAC Value & Growth Channels Warriors</p>
                 </div>
             </div>
         </div>
